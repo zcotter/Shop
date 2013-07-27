c3447212c7f54de61524814bc326e257c27520b2
Zach Cotter <cotter.z@husky.neu.edu>
2013-07-27 18:44:42 -0400

    Added category field to form and show

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :products
   attr_accessible :name
+  def get_name
+    "#{name}"
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price, :quantity
+  attr_accessible :description, :name, :price, :quantity, :category_id
   has_many :pictures
   belongs_to :category
 end
