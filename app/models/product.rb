0d2556089344141577d427733dde09becd7f0e9d
Zach Cotter <cotter.z@husky.neu.edu>
2013-08-02 23:41:46 -0400

    Added search function to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :quantity, :category_id
   has_many :pictures
   belongs_to :category
+
+  def self.search(searchTerm)
+    if searchTerm
+      find(:all, :conditions => ['name like ?', "%#{searchTerm}%"])
+    else
+      find(:all)
+    end
+  end
 end
+
