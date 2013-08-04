6d4e86532c3ad24529f0b67780db8848510edf16
Zach Cotter <cotter.z@husky.neu.edu>
2013-08-04 00:12:41 -0400

    Changed thumbnail size for logo so it fits in navbar

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,6 @@
 class Store < ActiveRecord::Base
   attr_accessible :description, :email, :location, :name, :owner, :logo, :cover
-  has_attached_file :logo, :styles => { :small => "50x50>", :large => "100x100>" }
+  has_attached_file :logo, :styles => { :small => "27x27>", :large => "100x100>" }
   has_attached_file :cover, :styles => { :large => "2000x2000>" }
 
 end
