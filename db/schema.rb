66d75a586fdf1e50879c63f98c6bf31ba925a138
Zach Cotter <cotter.z@husky.neu.edu>
2013-08-03 20:30:37 -0400

    ran db:migrate for store scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130720231333) do
+ActiveRecord::Schema.define(:version => 20130803215647) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(:version => 20130720231333) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "category_id"
+  end
+
+  create_table "stores", :force => true do |t|
+    t.string   "name"
+    t.string   "owner"
+    t.string   "email"
+    t.string   "location"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
