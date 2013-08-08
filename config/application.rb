122b4fff099ef3e766cb4ca6025bb7e035844fa1
Zach Cotter <cotter.z@husky.neu.edu>
2013-08-07 20:19:19 -0400

    More attempts at dynamic styling

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,14 @@ if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  #Bundler.require(:default, :assets, Rails.env)
 end
 
+
+
+
 module Shop
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
