8d3b6a7a3340850375349ba679e7d8a5cf8a3238
Zach Cotter <cotter.z@husky.neu.edu>
2013-08-08 21:47:19 -0400

    started runtime compilation of less after changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
-  def style_tag(stylesheet)
-    asset = Shop::Application.assets[stylesheet]
-    environment = asset.instance_variable_get('@environment')
-    logical_path = asset.logical_path
-    pathname = asset.pathname
-    clone = asset.class.new(logical_path, pathname, {})
-    content_tag("STYLE", clone.body.html_safe, type: "text/css")
-  end
+
 end
